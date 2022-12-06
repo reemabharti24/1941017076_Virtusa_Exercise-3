26230e25bf7ea91c1d531f6a4782028a0985fd21
reemabharti24 <53610950+reemabharti24@users.noreply.github.com>
2022-12-06 23:43:24 +0530

    Initial commit

---
+++ src/main/java/com/example/Springboot/VirtusaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtusaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtusaApplication.class, args);
+	}
+
+}
